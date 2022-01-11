481db940bbf00d48835054a7d027dd1164557282
ryunseok <ryunseok@gmail.com>
2022-01-11 14:14:27 +0900

    mysql local db 연결 및 users entity정의

---
+++ src/modules/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "src/entities/user.entity";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User]])],
+  providers: [UsersService],
+  controllers: [UsersController],
+})
+
+export class UsersModule {}
